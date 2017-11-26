4b332b7271d7de590d6cedd11c5fa6e9cfb6e3c8
Krystian Trojanowski <ktrojanowski@hso.grupa.skok>
2017-11-26 01:50:11 +0100

    Obsluga Google Maps Api i dodanie ActionBar jako menu

--- app/src/main/java/pl/store/geekmarket/MainActivity.java
+++ app/src/main/java/pl/store/geekmarket/MainActivity.java
@@ -1,7 +1,12 @@
 package pl.store.geekmarket;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("GeekMarket ");
+        actionBar.setIcon(R.mipmap.geek_market_logo);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch(item.getItemId()) {
+            case R.id.products:
+                Toast.makeText(this, "Clicked Products!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.manager:
+                Toast.makeText(this, "Clicked Manager!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.map:
+                Toast.makeText(this, "Clicked Map!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, MapsActivity.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/pl/store/geekmarket/MapsActivity.java
@@ -0,0 +1,62 @@
+package pl.store.geekmarket;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final LatLng GEEKMARKET_CENTER = new LatLng(54.331692, 18.633125);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps_activity);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(" GeekMarket - Map ");
+        actionBar.setIcon(R.mipmap.geek_market_logo);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.geek_market_logo)).anchor(0.0f,1.0f).title("GeekMarket").position(GEEKMARKET_CENTER));
+        CameraPosition cameraPosistion = new CameraPosition.Builder().target(GEEKMARKET_CENTER).zoom(15).bearing(0).tilt(30).build();
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosistion));
+
+    }
+}
